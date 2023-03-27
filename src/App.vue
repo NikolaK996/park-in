4d02cd51a120462bc1aca7b5d786d0616dcc968e
kurdu <kurdumanovic49@gmail.com>
2023-03-28 01:03:07 +0200

    project initialized using vue-cli

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <TheHeader />
+  <RouterView />
+</template>
+
+<script setup>
+import TheHeader from "@/components/base/navigation/TheHeader.vue"
+import { RouterView } from 'vue-router'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <div>Home page</div>
+  </main>
+</template>
