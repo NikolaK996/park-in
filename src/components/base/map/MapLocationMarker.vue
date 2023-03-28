ed702b39790103c84107546c78fd00b28fad8654
kurdu <kurdumanovic49@gmail.com>
2023-03-28 23:02:42 +0200

    [PAR-5]: Added multiple markers depend on area

---
+++ src/components/base/map/MapLocationMarker.vue
@@ -0,0 +1,25 @@
+<template>
+  <l-marker :lat-lng="latLng"/>
+</template>
+
+<script>
+import {LMarker} from "@vue-leaflet/vue-leaflet";
+
+export default {
+  name: "MapLocationMarker",
+  components: {
+    LMarker
+  },
+  props: {
+    latLng: {
+      type: Array,
+      required: true,
+      default: () => [0, 0]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
