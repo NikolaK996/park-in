07837ef15c7e26359ab31b832efbd8f59b0f551d
kurdu <kurdumanovic49@gmail.com>
2023-04-04 13:42:28 +0200

    [PAR-14]: Input and map search are created

---
+++ src/components/base/inputs/InputSearch.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input v-model="searchTerm" type="text" />
+    <button @click="search">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputSearch',
+  emits: ['search'],
+  data() {
+    return {
+      searchTerm: ''
+    }
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.searchTerm)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
