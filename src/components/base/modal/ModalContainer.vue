d5b68b181762fec07e72aa8a9c3f3e02fe75bd4d
kurdu <kurdumanovic49@gmail.com>
2023-04-17 22:29:03 +0200

    [PAR-124]: Created voice input in search

--- src/components/base/modal/ModalContainer.vue
+++ src/components/base/modal/ModalContainer.vue
@@ -1,9 +1,9 @@
 <template>
   <section
-    class="fixed backdrop-blur-sm flex items-center justify-center top-0 left-0 w-full h-full"
+    class="fixed backdrop-blur-sm flex items-center justify-center top-0 left-0 w-full h-full min-w-[64rem]"
     @click="emit('close')"
   >
-    <div class="bg-white" @click.stop>
+    <div class="modal-content bg-white shadow-xl" @click.stop>
       <slot />
     </div>
   </section>
@@ -16,5 +16,9 @@ const emit = defineEmits(['close'])
 <style lang="scss" scoped>
 section {
   z-index: 10000;
+}
+
+.modal-content {
+  min-width: 32rem;
 }
 </style>
